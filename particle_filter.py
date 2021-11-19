f0022b0955b35df3429848b3cd3aeb34173e868f
greatsharma <gsharma81999@gmail.com>
2021-11-19 17:59:59 +0530

    bug correction in resampling, added gaussian measurement noise and improved visualization

--- particle_filter.py
+++ particle_filter.py
@@ -3,8 +3,8 @@ import cv2
 import time
 import math
 import shutil
-from collections import deque
 import random
+from collections import deque
 from itertools import product
 
 import matplotlib.pyplot as plt
@@ -14,12 +14,12 @@ MIN_XRANGE = 1
 MIN_YRANGE = 1
 MAX_XRANGE = 50
 MAX_YRANGE = 50
-TRUE_CLOSEST_POINT = [25, 25]
+GOAL_POINT = [25, 25]
 
 
-def do_measurement(particles):
+def do_measurement(particles, measurement_noise=0):
     for idx in range(len(particles)):
-        particles[idx][2] = math.sqrt((particles[idx][0]-TRUE_CLOSEST_POINT[0])**2 + (particles[idx][1]-TRUE_CLOSEST_POINT[1])**2) + random.randint(0,2)
+        particles[idx][2] = abs(math.sqrt((particles[idx][0]-GOAL_POINT[0])**2 + (particles[idx][1]-GOAL_POINT[1])**2) + random.gauss(0.0, measurement_noise))
     return particles
 
 
@@ -29,44 +29,57 @@ def update_weights(particles):
     return [w/weights_sum for w in weights]
 
 
-def resample(particles, num_particles, randomness=0):
-    particles = random.choices(particles, weights, k=num_particles)
-    return [[p[0]+random.randint(-randomness,randomness), p[1]+random.randint(-randomness,randomness), p[2]] for p in particles]
+def resample(particles, weights, num_particles, sampling_variance=0):
+    particles_indices = random.choices(range(len(particles)), weights, k=num_particles)
+
+    sampled_particles = []
+
+    for idx in particles_indices:
+        x = particles[idx][0] + random.gauss(0.0, sampling_variance)
+        y = particles[idx][1] + random.gauss(0.0, sampling_variance)
+        d = abs(math.sqrt((x-GOAL_POINT[0])**2 + (y-GOAL_POINT[1])**2) + random.gauss(0.0, sampling_variance))
+        sampled_particles.append([x, y, d])
+
+    return sampled_particles
 
 
-def plot_particles(particles):
-    x_coords = [p[0] + random.random() * random.choices([-1, 1], [1, 1])[0] for p in particles]
-    y_coords = [p[1] + random.random() * random.choices([-1, 1], [1, 1])[0] for p in particles]
-    plt.scatter(x_coords, y_coords, s=5, alpha=0.5)
-    plt.scatter(TRUE_CLOSEST_POINT[0], TRUE_CLOSEST_POINT[1], s=5, c="r")
-    # x_coords = [p[0] for p in particles]
-    # y_coords = [p[1] for p in particles]
-    # plt.scatter(x_coords, y_coords, s=2, c='black')
+def plot_particles(particles, iter_num, avg_dist_from_goal, global_avgcoord):
+    x_coords = [p[0] for p in particles]
+    y_coords = [p[1] for p in particles]
+    plt.scatter(x_coords, y_coords, s=3, alpha=0.25)
+    plt.scatter(sum(x_coords)/len(particles), sum(y_coords)/len(particles), s=6, color="black")
+
+    plt.scatter(GOAL_POINT[0], GOAL_POINT[1], s=6, c="r")
+
+    plt.annotate(f"iteration {iter_num}", (-2, 52), color="r")
+    plt.annotate(f"{len(particles)} Particles", (-2, 49), color="r")
+    plt.annotate(f"avg distance from goal is {avg_dist_from_goal}", (20, 52), color="g")
+    plt.annotate(f"global coord is {global_avgcoord}", (20, 49), color="g")
+
     ax = plt.gca()
     ax.set_xlim([MIN_XRANGE-5, MAX_XRANGE+5])
     ax.set_ylim([MIN_YRANGE-5, MAX_YRANGE+5])
 
+    plt.savefig(f"{dir_path}{iter_}.jpg")
+    plt.savefig(f"particle_filter.jpg")
+    plt.close()
+
 
 if __name__ == "__main__":
-    dir_path = "temp_images/"
+    dir_path = ".temp_images/"
+    if os.path.isdir(dir_path):
+        shutil.rmtree(dir_path)
     os.mkdir(dir_path)
 
-    num_particles = 100
+    num_iter = 50
+    num_particles = 250
 
-    x = random.choices(range(MIN_XRANGE, MAX_XRANGE), [1] * (MAX_XRANGE - MIN_XRANGE), k=int(math.sqrt(num_particles)))
-    y = random.choices(range(MIN_YRANGE, MAX_YRANGE), [1] * (MAX_YRANGE - MIN_YRANGE), k=int(math.sqrt(num_particles)))
-
-    distances = [100 for _ in range(num_particles)]
-    avgdist_list = deque()
-
+    particles = [[random.randint(MIN_XRANGE, MAX_XRANGE), random.randint(MIN_YRANGE, MAX_YRANGE), 100] for _ in range(num_particles)]
     weights = [1/num_particles for _ in range(num_particles)]
 
-    particles = list(product(x, y))
-    particles = [[p[0], p[1], d] for p,d in zip(particles, distances)]
+    avgdist_list = deque()
 
-    fig = plt.figure(figsize=(6, 6))
-
-    for i in range(100):
+    for iter_ in range(num_iter):
         if len(avgdist_list) < 6:
             avgdist_list.append(sum([p[2] for p in particles])/ num_particles)
         else:
@@ -78,41 +91,26 @@ if __name__ == "__main__":
             round(sum([p[1] for p in particles]) / len(particles), 2)
         )
 
-        print(f"iteration {i}: avg distance from goal is {round(avgdist_list[-1], 4)}: global coord is {global_avgcoord}")
+        plot_particles(particles, iter_, round(avgdist_list[-1], 4), global_avgcoord)
 
-        plot_particles(particles)
-        plt.annotate(f"iteration {i}", (-2, 52), color="r")
-        plt.annotate(f"{num_particles} Particles", (-2, 49), color="r")
-        plt.annotate(f"avg distance from goal is {round(avgdist_list[-1], 4)}", (20, 52), color="g")
-        plt.annotate(f"global coord is {global_avgcoord}", (20, 49), color="g")
-
-        if avgdist_list[-1] < 2:
-            plt.title("Converged")
+        if avgdist_list[-1] < 1.2:
             print("Converged !")
             break
         
-        plt.savefig(f"{dir_path}{i}.jpg")
-        plt.savefig(f"particle_filter.jpg")
-        plt.close()
-
         if len(avgdist_list) < 8:
-            particles = resample(particles, num_particles=100, randomness=1)
+            particles = resample(particles, weights, num_particles=num_particles, sampling_variance=0.5)
         elif avgdist_list[-1] < sum(avgdist_list)/len(avgdist_list):
             num_particles = max(50, num_particles - int(num_particles * 0.1))
-            particles = resample(particles, num_particles=num_particles)
+            particles = resample(particles, weights, num_particles=num_particles, sampling_variance=0)
         else:
             num_particles = min(200, num_particles + int(num_particles * 0.2))
-            particles = resample(particles, num_particles=num_particles, randomness=2)
+            particles = resample(particles, weights, num_particles=num_particles, sampling_variance=1.5)
 
-        particles = do_measurement(particles)
+        particles = do_measurement(particles, measurement_noise=1)
 
         weights = update_weights(particles)
 
         time.sleep(0.5)
-
-    plt.savefig(f"{dir_path}{i}.jpg")
-    plt.savefig(f"particle_filter.jpg")
-    plt.close()
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     videowriter = cv2.VideoWriter("particle_filter.mp4", fourcc, 1, (640,480))
